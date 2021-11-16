34933ecf4b691759a179e688db81b81b25bc6187
Rexford Machu <rexfordmachu@gmail.com>
2021-11-16 13:59:17 +0000

    Initial Commit

---
+++ Ladder-Clone/Ladder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ladder_CloneApp.swift
+//  Ladder-Clone
+//
+//  Created by Rexford Machu on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Ladder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
